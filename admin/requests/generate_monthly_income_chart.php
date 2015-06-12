50add93bff4bcec0e264c9f789c5c9a7c3c5e63d
Karl Macadangdang <karljarren0308@yahoo.com>
2015-06-13 01:00:45 +0800

    admin-side files

---
+++ admin/requests/generate_monthly_income_chart.php
@@ -0,0 +1,16 @@
+<?php
+    require('connection.php');
+    $action = mysqli_real_escape_string($connection, $_POST['action']);
+
+    if($action == 'renderGraph') {
+        $graph = mysqli_real_escape_string($connection, $_POST['graph']);
+
+        list($type, $graph) = explode(';', $graph);
+        list($base64, $graph) = explode(',', $graph);
+        $graph = base64_decode($graph);
+
+        file_put_contents('../assets/img/graph_report[' . date('Y-m-d') . '].png', $graph);
+    }
+
+    mysqli_close($connection);
+?>
