9453a670102119e7ed253115442eda9d7fae22af
Karl Macadangdang <karljarren0308@yahoo.com>
2015-06-13 03:35:54 +0800

    NoScript tag Added

--- assets/system/footer.php
+++ assets/system/footer.php
@@ -1,2 +1,19 @@
+<noscript>
+    <div class="overlay">
+        <div class="overlay">
+            <div class="overlay">
+                <div class="overlay-block">
+                    <div class="overlay-title">Oops! JavaScript was blocked on this page</div>
+                    <div class="overlay-body">
+                        <p>We're sorry, but this page won't work properly without JavaScript enabled. Please allow this page to run JavaScript and then reload the page.</p>
+                        <div class="text-right">
+                            <a href="" class="btn btn-primary btn-sm">Reload Page</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</noscript>
 </body>
 </html>
