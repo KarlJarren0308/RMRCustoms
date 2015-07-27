29f288f6cafedb009869b63149c68a84d820913a
Karl Macadangdang <karljarren0308@yahoo.com>
2015-07-28 02:35:04 +0800

    New Update(s)

--- index.php
+++ index.php
@@ -10,7 +10,41 @@
 
 <div class="container fg-white">
     <div class="col-lg-12 col-md-12">
-        <!-- Place Logo Here -->
+        <div id="rmr-carousel" class="carousel slide" data-ride="carousel" style="overflow: hidden; height: 300px;">
+            <ol class="carousel-indicators">
+                <li data-target="#rmr-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#rmr-carousel" data-slide-to="1"></li>
+                <li data-target="#rmr-carousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="assets/img/carousel01.jpg" style="margin-top: -200px; width: 100%;">
+                    <div class="carousel-caption">
+                        <h3>RMR Customs</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="assets/img/carousel01.jpg" style="margin-top: -200px; width: 100%;">
+                    <div class="carousel-caption">
+                        <h3>RMR Customs</h3>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="assets/img/carousel01.jpg" style="margin-top: -200px; width: 100%;">
+                    <div class="carousel-caption">
+                        <h3>RMR Customs</h3>
+                    </div>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#rmr-carousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#rmr-carousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
     <div class="col-lg-4 col-md-4 text-center">
         <br>
